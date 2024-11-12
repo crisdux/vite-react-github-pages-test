4a1c196f864c6379b8e67817642a0a1c9f26cf0c
crisdux <crisfer.4217@gmail.com>
2024-11-12 16:59:52 -0400

    se configuro el vite.config para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://crisdux.github.io/vite-react-github-pages-test"
 })
